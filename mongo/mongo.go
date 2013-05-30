da9379c355d8865cfbbe4f3f3282db650dbe0d41
Robin Monjo <robin.monjo@applidget.com>
2013-05-30 22:42:27 +0200

    testing

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -11,31 +11,19 @@ const dbName = "db_sort"
 const bubbleSortCollection = "bubble_sort"
 
 //global session
-var session *mgo.Session
+//var session *mgo.Session
 
-func InitSession() {
-	if (session != nil) {
-		fmt.Println("[WARNING] - Trying to init an already initialised MongoDB session")
-		return
-	}
-	var err error
-	session, err = mgo.Dial(os.Getenv("MONGOHQ_URL"))
+
+func InsertBubbleSortResult(jsonResult map[string]interface{}) {
+	session, err := mgo.Dial(os.Getenv("MONGOHQ_URL"))
 	if (err != nil) {
 		panic(err)
 	}
-	fmt.Println("[INFO] - session opened")
-}
-
-func CloseSession() {
-	session.Close()
-	fmt.Println("[INFO] - session closed")
-}
-
-func InsertBubbleSortResult(jsonResult map[string]interface{}) {
+	defer session.Close()
 	fmt.Println("[INFO] - inserting", jsonResult)
 	c := session.DB(dbName).C(bubbleSortCollection)
 	fmt.Println("[INFO] - collection", c)
-	err := c.Insert(jsonResult)
+	err = c.Insert(jsonResult)
   if err != nil {
 		panic(err)
 	}
